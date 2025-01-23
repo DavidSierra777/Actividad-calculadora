325fc757ce20800025fb3e9dbb48054b1e159243
DavidSierra777 <20231188@uthh.edu.mx>
2025-01-23 09:39:08 -0600

    Add files via upload

---
+++ funciones.ts
@@ -0,0 +1,27 @@
+function calculate(operation: string): void {
+    const num1 = parseFloat((document.getElementById("num1") as HTMLInputElement).value);
+    const num2 = parseFloat((document.getElementById("num2") as HTMLInputElement).value);
+    let resultado: string | number=0;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        resultado = "Ingresar valores correctos.";
+    } 
+    else {
+        if (operation === "suma") {
+            resultado = num1 + num2;
+        } 
+        else if (operation === "resta") {
+            resultado = num1 - num2;
+        } 
+        else if (operation === "multiplicacion") {
+            resultado = num1 * num2;
+        } 
+        else if (operation === "division") {
+            resultado = num2 !== 0 ? num1 / num2 : "Error matemático";
+        }
+    }
+    document.getElementById("resultado")!.innerText = `Resultado: ${resultado}`;
+}
+
+
+
